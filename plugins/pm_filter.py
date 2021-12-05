acc617209771b3e95ae8ed0b5f45dbdb68bc30ff
Ramesh Kumar Sahu <93857696+Film4Free@users.noreply.github.com>
2021-12-05 15:47:10 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -436,7 +436,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
             InlineKeyboardButton('👨‍👨‍👧 Join Group', url='https://t.me/+RVUzWLee4lxiYWNl')
             ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
+            InlineKeyboardButton('👨‍💻 Creator', url='https://t.me/being_ramesh'),
             InlineKeyboardButton('🤖 Updates', url='https://t.me/+xJxBiuMIvS9mNGY1')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
